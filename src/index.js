0245bd9b20bc26c6904ec50f4646cc5261d56c5e
Ajay <ajayk97.ak10@gmail.com>
2020-11-16 12:34:48 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
-/*import App from './App'*/
-/*import Maps from './Maps'*/
-/*import Todo from './Todo'*/
-/*import Hooks from './Hooks'*/
+
 import Form from './Form'
 
 /*console.log(Mobiles[0].title)*/
